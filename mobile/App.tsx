326c0d863f0a15769b2af0cf939db7ba26ac4bd4
Your Name <thabdoul0512@gmail.com>
2026-08-02 17:14:25 +0200

    point d'entree provisoire pour visualiser le selecteur

---
+++ mobile/App.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+import { SelecteurHumeur } from "./src/components/SelecteurHumeur";
+import { NiveauHumeur } from "./src/types/typeHumer";
+
+export default function App() {
+  const [valeur, setValeur] = useState<NiveauHumeur | null>(null);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <SelecteurHumeur value={valeur} onChange={setValeur} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20,
+  },
+});
